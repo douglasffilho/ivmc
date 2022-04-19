1e20028ad59a633a07ea21df6c2989c2797388fb
Douglas Fernandes <douglasf.filho@gmail.com>
2022-04-19 16:40:59 -0300

    feat: 🎸 add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import { PageTemplate } from 'components';
 const Home = () => {
   return (
     <PageTemplate>
+      <h1>
+        Um lugar pra encontrar opinões fortemente embasadas e pautas nas
+        multiplas vozes geniais da minha cabecinha.
+      </h1>
       <h1>Página em construção (pelo menos na minha cabeça)</h1>
     </PageTemplate>
   );
