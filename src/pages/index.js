7094b4cd69cd9d890d014b7ad406b6d96f773624
Douglas Fernandes <douglasf.filho@gmail.com>
2022-04-19 16:41:52 -0300

    fix: 🐛 main page spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ const Home = () => {
         Um lugar pra encontrar opinões fortemente embasadas e pautas nas
         multiplas vozes geniais da minha cabecinha.
       </h1>
+      <br />
+      <br />
+      <br />
       <h1>Página em construção (pelo menos na minha cabeça)</h1>
     </PageTemplate>
   );
